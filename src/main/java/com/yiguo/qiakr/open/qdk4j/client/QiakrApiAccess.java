9fbe721b566b63bd27e54d9bbdc3dcaa178fe0cd
yhzdys <yhzdys@foxmail.com>
2020-05-08 21:27:19 +0800

    优化部分代码结构

--- src/main/java/com/yiguo/qiakr/open/qdk4j/client/QiakrApiAccess.java
+++ src/main/java/com/yiguo/qiakr/open/qdk4j/client/QiakrApiAccess.java
@@ -41,81 +41,81 @@ public class QiakrApiAccess {
     }
 
     public AccessTokenResp getAccessToken() {
-        return client.getAccessToken();
+        return this.client.getAccessToken();
     }
 
     /* 门店相关接口 */
     public GetSupplierStoresResp getSupplierStores(String accessToken) {
-        return client.doRequest(
+        return this.client.doRequest(
                 "/store/getSupplierStores.json", accessToken, new BaseReq(), null, GetSupplierStoresResp.class
         );
     }
 
     /* 商品相关接口 */
     public GetBrandListResp getBrandList(GetBrandListReq req, String accessToken) {
-        return client.doRequest(
+        return this.client.doRequest(
                 "/brand/getBrandList.json", accessToken, req, this.STR_FEATURE, GetBrandListResp.class
         );
     }
 
     public GetCategoryListResp getCategoryList(GetCategoryListReq req, String accessToken) {
-        return client.doRequest(
+        return this.client.doRequest(
                 "/category/getCategoryList.json", accessToken, req, null, GetCategoryListResp.class
         );
     }
 
     public CreateSupplierStockResp createSupplierStock(CreateSupplierStockReq req, String accessToken) {
-        return client.doRequest(
+        return this.client.doRequest(
                 "/stock/createSupplierStock.json", accessToken, req, this.STR_FEATURE, CreateSupplierStockResp.class
         );
     }
 
     public BaseResp batchUpdateSupplierStock(BatchUpdateSupplierStockReq req, String accessToken) {
-        return client.doRequest(
+        return this.client.doRequest(
                 "/stock/batchUpdateSupplierStock.json", accessToken, req, null, BaseResp.class
         );
     }
 
     public BaseResp batchUpdateStoreStock(BatchUpdateStoreStockReq req, String accessToken) {
-        return client.doRequest(
+        return this.client.doRequest(
                 "/stock/batchUpdateStoreStock.json", accessToken, req, null, BaseResp.class
         );
     }
 
     /* 会员相关接口 */
     public BatchUpdateCustomerPointResp batchUpdateCustomerPoint(BatchUpdateCustomerPointReq req, String accessToken) {
-        return client.doRequest(
+        return this.client.doRequest(
                 "/customer/batchUpdateCustomerPoint.json", accessToken, req, null, BatchUpdateCustomerPointResp.class
         );
     }
 
     public BatchUpdateCustomerVipLevelResp batchUpdateCustomerVipLevel(BatchUpdateCustomerVipLevelReq req, String accessToken) {
-        return client.doRequest(
+        return this.client.doRequest(
                 "/customer/batchUpdateCustomerVipLevel.json", accessToken, req, null, BatchUpdateCustomerVipLevelResp.class
         );
     }
 
     /* 订单相关接口 */
     public BatchImportExtOrderResp batchImportExtOrder(BatchImportExtOrderReq req, String accessToken) {
-        return client.doRequest(
+        return this.client.doRequest(
                 "/order/batchImportExtOrder.json", accessToken, req, null, BatchImportExtOrderResp.class
         );
     }
 
     public GetOrderListResp getOrderList(GetOrderListReq req, String accessToken) {
-        return client.doRequest(
+        return this.client.doRequest(
                 "/order/getOrderList.json", accessToken, req, null, GetOrderListResp.class
         );
     }
 
     public GetRefundOrderListResp getRefundOrderList(GetRefundOrderListReq req, String accessToken) {
-        return client.doRequest(
+        return this.client.doRequest(
                 "/order/getRefundOrderList.json", accessToken, req, this.STR_FEATURE, GetRefundOrderListResp.class
         );
     }
 
     public BaseResp updateOrderToDeliveryStatus(UpdateOrderToDeliveryStatusReq req, String accessToken) {
-        return client.doRequest(
+        return this.client.doRequest(
                 "/order/updateOrderToDeliveryStatus.json", accessToken, req, this.STR_FEATURE, BaseResp.class
         );
     }
