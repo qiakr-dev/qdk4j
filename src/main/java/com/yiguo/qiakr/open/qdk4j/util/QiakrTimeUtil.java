49d2e194fb2b526e7198a10e0d40822b7cca91ab
yhzdys <yhzdys@foxmail.com>
2020-09-10 11:29:25 +0800

    clean code

--- src/main/java/com/yiguo/qiakr/open/qdk4j/util/QiakrTimeUtil.java
+++ src/main/java/com/yiguo/qiakr/open/qdk4j/util/QiakrTimeUtil.java
@@ -1,7 +1,6 @@
 package com.yiguo.qiakr.open.qdk4j.util;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -37,27 +36,4 @@ public final class QiakrTimeUtil {
     public static Date addSecond(Date date, int second) {
         return new Date(date.getTime() + (second * 1000L));
     }
-
-    public static Integer getAgeByBirth(Date birthday) {
-        int age = 0;
-        try {
-            Calendar now = Calendar.getInstance();
-            now.setTime(new Date());// 当前时间
-
-            Calendar birth = Calendar.getInstance();
-            birth.setTime(birthday);
-
-            if (birth.after(now)) {//如果传入的时间，在当前时间的后面，返回0岁
-                return age;
-            }
-            age = now.get(Calendar.YEAR) - birth.get(Calendar.YEAR);
-            if (now.get(Calendar.DAY_OF_YEAR) > birth.get(Calendar.DAY_OF_YEAR)) {
-                age += 1;
-            }
-            return age;
-        } catch (Exception e) {//兼容性更强,异常后返回数据
-            return 0;
-        }
-    }
-
 }
